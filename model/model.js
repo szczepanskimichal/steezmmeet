293d4322f81fdf8ff814abeaa12c5679aeb715ca
szczepanskimichal <127307935+szczepanskimichal@users.noreply.github.com>
2025-03-26 11:14:33 +0100

    modellskisse

---
+++ model/model.js
@@ -0,0 +1,20 @@
+const model = {
+ app:{
+    currentPage: 'home',
+    pages: ['home', 'about', 'contact']
+  },
+  inputs: {
+    name: '',
+    email: '',
+    message: ''
+  },
+  data: {
+    about: {
+      title: 'About',
+      content: 'This is the about page'
+    }
+ }
+
+
+
+};
