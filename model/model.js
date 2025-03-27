18fb63b4059b244b3ef7d8fc8e0c86f5df631a20
christis96 <christian.ulstein@gmail.com>
2025-03-27 14:53:19 +0100

    Update model.js

--- model/model.js
+++ model/model.js
@@ -1,83 +1,118 @@
 const model = {
- app:{
+  app: {
     currentPage: 'home',
-    pages: ['logInPage', 'registerPage', 'profilePage', 'dashboardPage', 'chatPage',  'createEventPage', 'viewEventPage', 'viewAllEvents', 'galleryPage', 'imagePage', 'videoPage','statisticsPage', 'viewAllUsers', 'viewFriendsPage', "adminPage", "galleryAdminPage", "vennListeAdmin"]
+    pages: [
+      'logInPage', 'registerPage', 'profilePage', 'dashboardPage', 'chatPage',
+      'createEventPage', 'viewEventPage', 'viewAllEvents', 'galleryPage',
+      'imagePage', 'videoPage', 'statisticsPage', 'viewAllUsers',
+      'viewFriendsPage', 'adminPage', 'galleryAdminPage', 'vennListeAdmin'
+    ]
   },
-  
   inputs: {
     logInPage: {
       username: '',
       password: '',
     },
-    
     registerPage: {
       username: '',
       password: '',
       confirmPassword: '',
-
     },
-
-    profilePage:{
-      username:'',
-      aboutMe:'',
-      favoriteSlope: '', 
-      favourteLocation: '',
-      status: false, // true, false, "custommade"
-      latestChats: [],
-    }, 
-
-    viewAllEventsPage:{ //Usikker på om det burde være input/data
-      title: '',
-      date: '', 
-      time: '',
-      location: '',
-    }, 
-
     viewEventPage: {
       title: '',
       date: '',
       place: '',
-      slope:'',
+      slope: '',
       extraInfo: '',
       attendees: [],
     },
-    editmode:{
+    editmode: {
       videoPage: {
         title: '',
         link: '',
         description: '',
       },
     },
-    
-      imagePage: {
-        title: '',
-        link: '',
-        description: '',
-      },
-    
-    statisticsPage:{
-      hoursInSlope:'',
-      daysInSlope:'',
-    }, 
-
-    viewAllFriendsPage:{
-      name:'',
-      status: true, 
-      bio: '', 
+    imagePage: {
+      title: '',
+      link: '',
+      description: '',
     },
   },
-
   data: {
     users: [
-      {id: 2, username: "user2"},
-    
+      {
+        id: 2,
+        username: "user2",
+        name: "user2name",
+        aboutMe: "jeg elsker ski",
+        favoriteSlopeID: [1],
+        favoriteLocationID: [2],
+        status: false,
+        chat: [
+          {
+            ToUserID: 1,
+            conversation: [
+              {
+                date: "11/11/11",
+                time: "10:08",
+                userID: 1,
+                message: "hei hei",
+              },
+              {
+                interactedUserID: 1,
+                conversation: [
+                  {
+                    date: "11/11/11",
+                    time: "10:15",
+                    userID: 2,
+                    message: "Hei på deg",
+                  },
+                ]
+              }
+            ]
+          }
+        ],
+        friendsID: [1, 3, 4],
+        statistics: [{}],
+      },
+      {
+        id: 1,
+        username: "user1",
+        name: "user1name",
+        aboutMe: "jeg elsker snowboard",
+        favoriteSlopeID: [3],
+        favoriteLocationID: [5],
+        status: true,
+        chat: [
+          {
+            ToUserID: 2,
+            conversation: [
+              {
+                date: "11/10/11",
+                time: "20:08",
+                userID: 1,
+                message: "hei hei",
+              },
+              {
+                interactedUserID: 1,
+                conversation: [
+                  {
+                    date: "09/0101",
+                    time: "10:15",
+                    userID: 2,
+                    message: "helloooooooo :)",
+                  },
+                ]
+              }
+            ]
+          }
+        ],
+        friendsID: [1, 3, 4],
+        statistics: [{}],
+      }
     ],
-    friends: [
-      {id: 1, username: "per"},
-      {id: 2, username: "pål"},
-      {id: 3, username: "espen askeladd"}
-
-  ], 
+    
     events: [
       {
         id: 1,
@@ -88,31 +123,15 @@ const model = {
         slope: "blue",
         extraInfo: "",
         attandees: [1, 2, 3],
-
       }
-    ], 
-    profilePage:{
-      username:'user2', 
-      aboutMe:'jeg liker å stå på ski', 
-      favoriteSlope: 'hafjell',  
-      favourteLocation: 'lillehammer', 
-      status: false, 
-      latestChats: [],
-    }, 
-
-    statisticsPage:{
-      id: 1,
-      creatorId: 2,
-      hoursInSlope:'92',
-      daysInSlope:'25',
-    }, 
+    ],
     about: {
       title: 'About',
       content: 'Den mest chille veien for å planlegge shreds med gjengen!'
-    }, 
+    },
     logo: {
       src: 'img/logo.png',
       alt: 'logo'
     },
-  }
+  },
 };
